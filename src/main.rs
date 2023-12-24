09138e0e39529d3f0cf8844ff1cc4bc0e8158957
Lain <145809470+Lain62@users.noreply.github.com>
2023-12-24 19:15:32 +0700

    added computer rng input function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::Rng;
 
 // function to get the player input and checks if it is correct
 fn get_player_input() -> String{
@@ -14,7 +15,20 @@ fn get_player_input() -> String{
     }
 }
 
+fn get_computer_input() -> String{
+    let computer_selection: u32 = rand::thread_rng().gen_range(1..=3);
+    match computer_selection {
+        1 => return "rock".to_string(),
+        2 => return "paper".to_string(),
+        3 => return "scissors".to_string(),
+        _ => return "Computer Calculation Error".to_string()
+    }
+
+}
+
 fn main() {
     let user_fighter = get_player_input();
-    println!("you choose {user_fighter}");
+    let computer_fighter = get_computer_input();
+    println!("you choose {}", user_fighter);
+    println!("The bot choose {}", computer_fighter)
 }
