555277e5c75b9445febf438f7c205f8ca953ec28
Lain <145809470+Lain62@users.noreply.github.com>
2024-01-02 22:28:10 +0700

    added game logic

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn get_player_input() -> String{
         io::stdin().read_line(&mut user_fighter).expect("User fighter input error");
         let user_fighter= user_fighter.trim().to_lowercase();
         match user_fighter.as_str() {
-            "rock" | "paper" | "scissors" => break user_fighter.to_uppercase(),
+            "rock" | "paper" | "scissors" => return user_fighter.to_uppercase(),
             _ => println!("Please enter a valid fighter.")
         }
     }
@@ -18,17 +18,37 @@ fn get_player_input() -> String{
 fn get_computer_input() -> String{
     let computer_selection: u32 = rand::thread_rng().gen_range(1..=3);
     match computer_selection {
-        1 => return "rock".to_string(),
-        2 => return "paper".to_string(),
-        3 => return "scissors".to_string(),
+        1 => return "ROCK".to_string(),
+        2 => return "PAPER".to_string(),
+        3 => return "SCISSORS".to_string(),
         _ => return "Computer Calculation Error".to_string()
     }
 
+}
+
+fn game_logic(user:String, computer:String) -> &'static str{
+    if &user == "ROCK" && &computer == "PAPER"{
+        return "WIN" 
+    } if &user == "ROCK" && &computer == "SCISSORS"{
+        return "LOSE" 
+    } if &user == "PAPER" && &computer == "ROCK"{
+        return "WIN" 
+    } if &user == "PAPER" && &computer == "SCISSORS"{
+        return "LOSE" 
+    } if &user == "SCISSORS" && &computer == "PAPER"{
+        return "WIN" 
+    } if &user == "SCISSORS" && &computer == "ROCK"{
+        return "LOSE" 
+    } else {
+        return "DRAW"
+    }
 }
 
 fn main() {
     let user_fighter = get_player_input();
     let computer_fighter = get_computer_input();
     println!("you choose {}", user_fighter);
-    println!("The bot choose {}", computer_fighter)
+    println!("The bot choose {}", computer_fighter);
+    let result = game_logic(user_fighter, computer_fighter);
+    println!("you {}", result);
 }
